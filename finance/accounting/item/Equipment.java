ee3db1392d48cfd6887d84ea6f41fbf8a88cdcb5
Ibusa-T <souichirou199513@gmail.com>
2025-07-12 01:15:22 +0900

    define equiment,  Merchandise

---
+++ finance/accounting/item/Equipment.java
@@ -0,0 +1,12 @@
+package item;
+
+/**
+ * @author 
+ * @Ibusa-T
+ * Equipment.class
+ * 備品の管理をするクラスです
+ * 例えば減価償却にある定額法、定率法
+ * また、間接法における減価償却累計額の計上をサポートします。
+ */
+
+final class Equipment{}
---
+++ finance/accounting/item/MerchandiseAdmin.java
@@ -0,0 +1,9 @@
+package accounting.item;
+
+/**
+ * @author 
+ * @Ibusa-T
+ * @Merchandise.class
+ * 商業簿記における繰越商品を扱うクラスです。
+ */
+final class Merchandise{}
